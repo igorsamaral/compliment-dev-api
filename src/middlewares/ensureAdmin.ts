6d84c56d14c4d380b9e0071751078cf3759c9761
Igor Santana <igr.amaral@gmail.com>
2021-07-30 08:26:05 -0300

    Criado middleware para verificar se usuário é admin

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
+
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    const { user_id } = request
+
+    const userRepository = getCustomRepository(UserRepository)
+
+    const { admin } = await userRepository.findOne(user_id)
+
+    if (admin) {
+        return next()
+    }
+
+    return response.status(401).json({error: "Acesso não autorizado"})
+}
