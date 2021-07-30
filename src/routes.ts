8e9a77591b8dad4f09e9c6fe11f74bdc1bc2d709
Igor Santana <igr.amaral@gmail.com>
2021-07-30 10:10:21 -0300

    Criado rotas de listagem

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,10 @@ import { ComplimentController } from "./controllers/ComplimentController"
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
+import { ListUserSentComplimentsController } from "./controllers/ListUserSentComplimentsController"
+import { ListUserReceivedComplimentsController } from "./controllers/ListUserReceivedComplimentsController"
+import { ListTagsController } from "./controllers/ListTagsController"
+import { ListUsersController } from "./controllers/ListUsersController"
 
 const router = Router()
 
@@ -13,10 +17,18 @@ const userController = new UserController()
 const tagController = new TagController()
 const complimentController = new ComplimentController()
 const authenticateUserController = new AuthenticateUserController()
+const listUserSentComplimentsController = new ListUserSentComplimentsController()
+const listUserReceivedComplimentsController = new ListUserReceivedComplimentsController()
+const listTagsController = new ListTagsController()
+const listUsersController = new ListUsersController()
 
 router.post("/users", userController.handle)
+router.get("/users", ensureAuthenticated, listUsersController.handle)
 router.post("/tags", ensureAuthenticated, ensureAdmin, tagController.handle)
+router.get("/tags", listTagsController.handle)
 router.post("/compliments", ensureAuthenticated, complimentController.handle)
+router.get("/users/compliments/sent", ensureAuthenticated, listUserSentComplimentsController.handle)
+router.get("/users/compliments/received", ensureAuthenticated, listUserReceivedComplimentsController.handle)
 router.post("/login", authenticateUserController.handle)
 
 export { router }
